b564cb4e6a6e64dbc9d8c5e603584a5dc55e3550
Grzegorz Greger <ligrew@gmail.com>
2012-10-04 17:32:34 +0200

    Projekt_0 - VS

---
+++ proj_0/triangle.cpp
@@ -0,0 +1,90 @@
+// Triangle.cpp
+// Our first OpenGL program that will just draw a triangle on the screen.
+
+#include <GLTools.h>            // OpenGL toolkit
+
+
+#ifdef __APPLE__
+#include <glut/glut.h>          // OS X version of GLUT
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>            // Windows FreeGlut equivalent
+#endif
+
+
+GLuint shader;
+
+///////////////////////////////////////////////////////////////////////////////
+// Window has changed size, or has just been created. In either case, we need
+// to use the window dimensions to set the viewport and the projection matrix.
+
+void ChangeSize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// This function does any needed initialization on the rendering context.
+// This is the first opportunity to do any OpenGL related tasks.
+
+void SetupRC()
+{
+    // Blue background
+    glClearColor(0.097f, 0.184f, 0.199f, 1.0f);
+
+	shader = gltLoadShaderPairWithAttributes("shader.vp", "shader.fp",
+		2, GLT_ATTRIBUTE_VERTEX, "vVertex", GLT_ATTRIBUTE_COLOR, "vColor");
+    fprintf(stdout, "GLT_ATTRIBUTE_VERTEX : %d\nGLT_ATTRIBUTE_COLOR : %d \n",
+		GLT_ATTRIBUTE_VERTEX, GLT_ATTRIBUTE_COLOR);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Called to draw scene
+
+void RenderScene(void)
+{
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+   
+    glUseProgram(shader);
+
+	glBegin(GL_TRIANGLES);
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.184, 0.598, 0.754);
+		
+		glVertex3f(-0.5f, -0.25f, 1.0f);
+		glVertex3f(0.5f, -0.25f, 1.0f);
+		glVertex3f(-0.5f, 0.5f, 1.0f);
+	glEnd();
+
+    // Perform the buffer swap to display back buffer
+    glutSwapBuffers();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Main entry point for GLUT based programs
+
+int main(int argc, char* argv[]) {
+  
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    glutInitWindowSize(1024, 768);
+    glutCreateWindow("Trójk¹t");
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+
+    SetupRC();
+
+    glutMainLoop();
+    return 0;
+}
