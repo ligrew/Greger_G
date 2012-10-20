da9d1d1de1d165279293d5cb207fd8f753d12d82
ligrew <ligrew@gmail.com>
2012-10-20 04:38:10 -0700

    Projekt_1 - VS

---
+++ proj_1/VS/czworobok.cpp
@@ -0,0 +1,131 @@
+// Triangle.cpp
+// Our first OpenGL program that will just draw a triangle on the screen.
+
+#include <GLTools.h>            // OpenGL toolkit
+
+
+#ifdef __APPLE__
+#include <glut/glut.h>          // OS X version of GLUT
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>            // Windows FreeGlut equivalent
+#endif
+
+
+GLuint shader;
+
+///////////////////////////////////////////////////////////////////////////////
+// Window has changed size, or has just been created. In either case, we need
+// to use the window dimensions to set the viewport and the projection matrix.
+
+void ChangeSize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// This function does any needed initialization on the rendering context.
+// This is the first opportunity to do any OpenGL related tasks.
+
+void SetupRC()
+{
+    // Blue background
+    glClearColor(0.097f, 0.184f, 0.199f, 1.0f);
+
+	shader = gltLoadShaderPairWithAttributes("shader.vp", "shader.fp",
+		2, GLT_ATTRIBUTE_VERTEX, "vVertex", GLT_ATTRIBUTE_COLOR, "vColor");
+    fprintf(stdout, "GLT_ATTRIBUTE_VERTEX : %d\nGLT_ATTRIBUTE_COLOR : %d \n",
+		GLT_ATTRIBUTE_VERTEX, GLT_ATTRIBUTE_COLOR);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Called to draw scene
+
+void RenderScene(void)
+{
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+   
+    glUseProgram(shader);
+
+	glBegin(GL_TRIANGLES);//tyl
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.5, 0.5, 0.5);
+		
+		glVertex3f(0.5f, 0.0f, 0.0f);
+		glVertex3f(-0.25f, 0.0f, 0.0f);
+		glVertex3f(0.0f, 0.75f, 0.5f);
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);//dno prawe
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.0, 0.5, 0.5);
+		
+		glVertex3f(0.5f, 0.0f, 0.0f);
+		glVertex3f(-0.25f, 0.0f, 0.0f);
+		glVertex3f(0.25f, -0.5f, 1.0f);
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);//dno lewe
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.5, 0.5, 0.0);
+		
+		glVertex3f(-0.25f, 0.0f, 0.0f);
+		glVertex3f(0.25f, -0.5f, 1.0f);
+		glVertex3f(-0.5f, -0.5f, 0.0f);
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);//prawa
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.5, 0.0, 0.0);
+		
+		glVertex3f(0.5f, 0.0f, 0.0f);
+		glVertex3f(0.25f, -0.5f, 1.0f);
+		glVertex3f(0.0f, 0.75f, 0.5f);
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);//lewa
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.0, 0.0, 0.5);
+		
+		glVertex3f(-0.25f, 0.0f, 1.0f);
+		glVertex3f(-0.5f, -0.5f, 0.0f);
+		glVertex3f(0.0f, 0.75f, 0.5f);
+	glEnd();
+	
+	glBegin(GL_TRIANGLES);//przod
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.0, 0.5, 0.0);
+		
+		glVertex3f(0.25f, -0.5f, 1.0f);
+		glVertex3f(-0.5f, -0.5f, 0.0f);
+		glVertex3f(0.0f, 0.75f, 0.5f);
+	glEnd();
+	
+
+    // Perform the buffer swap to display back buffer
+    glutSwapBuffers();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Main entry point for GLUT based programs
+
+int main(int argc, char* argv[]) {
+  
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    glutInitWindowSize(1024, 768);
+    glutCreateWindow("Czworobok");
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+
+    SetupRC();
+
+    glutMainLoop();
+    return 0;
+}
