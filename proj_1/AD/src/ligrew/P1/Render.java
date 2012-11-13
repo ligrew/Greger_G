18c589286e9a22c36be4cfad854d25dc8e818c5b
ligrew <ligrew@gmail.com>
2012-11-13 10:19:10 -0800

    Projekt_1 - AD aktualizowany

--- proj_1/AD/src/ligrew/P1/Render.java
+++ proj_1/AD/src/ligrew/P1/Render.java
@@ -6,91 +6,117 @@ import javax.microedition.khronos.opengles.GL10;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 import android.util.Log;
 
-public class Render implements GLSurfaceView.Renderer {
-
+public class Render implements GLSurfaceView.Renderer
+{
     private static final String TAG = "Render";
     private Trojkat [] tabTrojkat = new Trojkat [6];
 
-    private final float[] mMVPMatrix = new float[16];
-    private final float[] mProjMatrix = new float[16];
-    private final float[] mVMatrix = new float[16];
-    private final float[] mRotationMatrix = new float[16];
+    private final float[] wynikM = new float[16];
+    private final float[] projekcjaM = new float[16];
+    private final float[] widokM = new float[16];
+    private final float[] rotacjaM = new float[16];
 
     public volatile float kat;	// Declare as volatile because we are updating it from another thread
+    public volatile float katN;
+    public volatile float x;
+    public volatile float y;
+    public volatile float z = 4;
+    public volatile boolean nacisk;
 
     public void onSurfaceCreated(GL10 unused, EGLConfig config)
     {
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
         GLES20.glClearColor(0.097f, 0.184f, 0.199f, 1.0f);	// kolor t³a
 
         //ty³
-        tabTrojkat[0] = new Trojkat(
-									 
-									 0.0f,  0.75f, 0.5f,
-									 0.5f,  0.0f,  0.0f,
-										-0.25f, 0.0f,  0.0f,
-           	 					 	 0.5f,  0.5f,  0.5f, 0.5f );
+        tabTrojkat[0] = new Trojkat(-1.0f, -1.0f, 0.0f,
+									 1.0f, -1.0f, 0.0f,
+									 0.0f, 0.0f, 2.0f,
+           	 					 	 0.5f,  0.5f,  0.5f, 1.0f );
         //dno prawe
-        tabTrojkat[1] = new Trojkat( 0.5f,   0.0f, 0.0f,
-        							-0.25f,  0.0f, 0.0f,
-        							 0.25f, -0.5f, 1.0f,
+        tabTrojkat[1] = new Trojkat( 1.0f, 1.0f, 0.0f,
+									 1.0f, -1.0f, 0.0f,
+									-1.0f, 1.0f, 0.0f,
            	 					 	 0.0f, 0.5f, 0.5f, 1.0f );
         //dno lewe
-        tabTrojkat[2] = new Trojkat(-0.25f, 0.0f, 0.0f,
-        							 0.25f, -0.5f, 1.0f,
-        							-0.5f, -0.5f, 1.0f,
+        tabTrojkat[2] = new Trojkat(-1.0f, 1.0f, 0.0f,
+									-1.0f, -1.0f, 0.0f,
+									 1.0f, -1.0f, 0.0f,
            	 					 	 0.5f, 0.5f, 0.0f, 1.0f );
         //prawa
-        tabTrojkat[3] = new Trojkat( 0.5f, 0.0f, 0.0f,
-        							 0.25f, -0.5f, 1.0f,
-        							 0.0f, 0.75f, 0.5f,
+        tabTrojkat[3] = new Trojkat( 1.0f, -1.0f, 0.0f,
+									 1.0f, 1.0f, 0.0f,
+									 0.0f, 0.0f, 2.0f,
            	 					 	 0.5f, 0.0f, 0.0f, 1.0f );
         //lewa
-        tabTrojkat[4] = new Trojkat(-0.25f, 0.0f, 0.0f,
-        							-0.5f, -0.5f, 1.0f,
-        							 0.0f, 0.75f, 0.5f,
+        tabTrojkat[4] = new Trojkat(-1.0f, 1.0f, 0.0f,
+									-1.0f, -1.0f, 0.0f,
+									 0.0f, 0.0f, 2.0f,
            	 					 	 0.0f, 0.0f, 0.5f, 1.0f );
         //przód
-        tabTrojkat[5] = new Trojkat(
-        							-0.5f, -0.5f, 1.0f,
-				 					 0.0f, 0.75f, 0.5f,
-				 					 0.25f, -0.5f, 1.0f,
+        tabTrojkat[5] = new Trojkat( 1.0f, 1.0f, 0.0f,
+									-1.0f, 1.0f, 0.0f,
+									 0.0f, 0.0f, 2.0f,
            	 					 	 0.0f, 0.5f, 0.0f, 1.0f );
     }
 
     public void onDrawFrame(GL10 unused)
     {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);		// ustaw kolor t³a
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);		// ustaw kolor t³a
 
-        Matrix.setLookAtM(mVMatrix, 0, 0, 0, 4, 0f, 0f, 0f, 0f, 1.0f, 0.0f);	// ustaw pozycjê kamery (View matrix)
+        Matrix.setLookAtM(widokM, 0, 0, 0, 1,		//kierunek
+        							 0, 0, 5.0f, 		//pozycja
+        							 0f, 1.0f, 0.0f);	//gora
 
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0, mVMatrix, 0);		// obliczanie transformacji projekcji i widoku
-
-        // Create a rotation for the triangle
-//        long time = SystemClock.uptimeMillis() % 4000L;
-//        float angle = 0.090f * ((int) time);
-        Matrix.setRotateM(mRotationMatrix, 0, kat/2, 0, 0, 1.0f);
+        Matrix.multiplyMM(wynikM, 0, projekcjaM, 0, widokM, 0);		// obliczanie transformacji projekcji i widoku
+        
+        if (z < 1)
+        	z = 1;
+        else if (z > 10)
+        	z=10;
+        
+        Matrix.translateM(wynikM, 0, 0, 0, z);
+        
+         //Create a rotation for the triangle
+        
+        Matrix.rotateM(wynikM, 0, x, 0, 1, 0);
+        Matrix.rotateM(wynikM, 0, y, 1, 0, 0);
+        long time = SystemClock.uptimeMillis() % 4000L;
+        kat = 0.090f * ((int) time);
+        
+        Matrix.setRotateM(rotacjaM, 0, kat, 0.0f, 0.0f, 1.0f);
 
         // Combine the rotation matrix with the projection and camera view
-        Matrix.multiplyMM(mMVPMatrix, 0, mRotationMatrix, 0, mMVPMatrix, 0);
+        Matrix.multiplyMM(wynikM, 0, rotacjaM, 0, wynikM, 0);
 
         // Draw triangle
-        for (int i =0; i < 6; i++)
-        	tabTrojkat[i].draw(mMVPMatrix);
+        for (int i=0;i<6;i++)
+        	tabTrojkat[i].draw(wynikM);
     }
 
+	public static void perspektywa(float[] mM, int mOffset, float f_fov, float f_aspect, float f_near, float f_far)
+	{
+		float f_w, f_h;
+
+		f_h = (float)(Math.tan(f_fov * Math.PI / 180 * .5f)) * f_near;
+		f_w = f_h * f_aspect;
+		// calc half width of frustum in x-y plane at z = f_near
+
+		Matrix.frustumM(mM, mOffset, -f_w, f_w, -f_h, f_h, f_near, f_far);
+		// set frustum
+	}
+	
     public void onSurfaceChanged(GL10 unused, int width, int height)
     {
-        // Adjust the viewport based on geometry changes,
-        // such as screen rotation
         GLES20.glViewport(0, 0, width, height);
 
         float ratio = (float) width / height;
-
-        // this projection matrix is applied to object coordinates
-        // in the onDrawFrame() method
-        Matrix.frustumM(mProjMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+        //perspektywa(projekcjaM, 0, 90, ratio, 1f, 1000.0f);
+        //Matrix.perspectiveM(projekcjaM, 0, 90, ratio, 1, 100);
+        Matrix.frustumM(projekcjaM, 0, -ratio, ratio, -1f, 1f, 1f, 10f);		//0, left, right, bottom, top, near, far
     }
 
     public static int loadShader(int type, String shaderCode)
--- proj_1/AD/src/ligrew/P1/Scena.java
+++ proj_1/AD/src/ligrew/P1/Scena.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.view.MotionEvent;
 
 public class Scena extends Activity
@@ -16,25 +17,22 @@ public class Scena extends Activity
         super.onCreate(savedInstanceState);
 
         widok = new Powierzchnia(this);		// stwórz now¹ instancjê GLSurfaceView
+
+        //widok.setEGLContextClientVersion(1);		// Za³ó¿ kontekst OpenGL ES 2.0
+
+        //widok.setRenderer(new Render());		// ustaw stworzony render jako render rysowania
         setContentView(widok);		// i ustaw go jako widok aplikacji
     }
 
     protected void onPause()
     {
         super.onPause();
-        // The following call pauses the rendering thread.
-        // If your OpenGL application is memory intensive,
-        // you should consider de-allocating objects that
-        // consume significant memory here.
         widok.onPause();
     }
 
     protected void onResume()
     {
         super.onResume();
-        // The following call resumes a paused rendering thread.
-        // If you de-allocated graphic objects for onPause()
-        // this is a good place to re-allocate them.
         widok.onResume();
     }
 }
@@ -52,45 +50,81 @@ class Powierzchnia extends GLSurfaceView
         render = new Render();
         setRenderer(render);		// ustaw stworzony render jako render rysowania
 
-        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);		// renderuj widok tylko wtedy, kiedy istnieje szansa narysowania danych rysowania
+        //setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);		// renderuj widok tylko wtedy, kiedy istnieje szansa narysowania danych rysowania
     }
 
     
-    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private final float TOUCH_SCALE_FACTOR = 0.5f;//90.0f / 320;
     private float poprzedniX;
     private float poprzedniY;
+    private float poprzedniZ;
+    private boolean nacisk = true;
 
-    public boolean onTouchEvent(MotionEvent e)
+
+	public boolean onTouchEvent(MotionEvent e)
     {
-        // MotionEvent reports input details from the touch screen
-        // and other input controls. In this case, you are only
-        // interested in events where the touch position changed.
+        float wskaznik = e.getPointerCount();
 
-        float x = e.getX();
-        float y = e.getY();
-
-        switch (e.getAction()) {
+        switch (e.getAction())
+        {
+	    	case MotionEvent.ACTION_UP:
+	    		nacisk = true;
+	    		break;
             case MotionEvent.ACTION_MOVE:
 
-                float dx = x - poprzedniX;
-                float dy = y - poprzedniY;
+            	if (wskaznik == 1)
+            	{
+                    float x = e.getX();
+                    float y = e.getY();
 
-                // reverse direction of rotation above the mid-line
-                if (y > getHeight() / 2) {
-                  dx = dx * -1 ;
-                }
+                    if (nacisk)
+                    {
+                    	poprzedniY = y;
+                    	poprzedniX = x;
+                    	nacisk = false;
+                    }
+                    
+	                float dx = x - poprzedniX;
+	                float dy = y - poprzedniY;
+	                // reverse direction of rotation above the mid-line
+	                if (y > getHeight() / 2)
+	                  dx = dx * -1 ;
+	
+	                // reverse direction of rotation to left of the mid-line
+	                if (x < getWidth() / 2)
+		                  dy = dy * -1 ;
+	
+	                render.x += dx * TOUCH_SCALE_FACTOR;
+	                render.y += dy * TOUCH_SCALE_FACTOR;
 
-                // reverse direction of rotation to left of the mid-line
-                if (x < getWidth() / 2) {
-                  dy = dy * -1 ;
-                }
-
-                render.kat += (dx + dy) * TOUCH_SCALE_FACTOR;  // = 180.0f / 320
+	                poprzedniX = x;
+	                poprzedniY = y;
+	                //render.katN += (dx + dy) * TOUCH_SCALE_FACTOR;  // = 180.0f / 320
+            	}
+            	else if (wskaznik == 2)
+            	{
+            		
+            		
+                    float x = e.getX(0) - e.getX(1);
+                    float y = e.getY(0) - e.getY(1);
+                    float z = FloatMath.sqrt(x * x + y * y);
+                    
+                    if (nacisk)
+                    {
+                    	poprzedniZ = z;
+                    	nacisk = false;
+                    }
+                    
+                    float dz = z - poprzedniZ;
+                    
+                    render.z -= dz * 0.02f;
+                    
+                    poprzedniZ = z;
+            	}
+            	
                 requestRender();
         }
 
-        poprzedniX = x;
-        poprzedniY = y;
         return true;
     }
 }
