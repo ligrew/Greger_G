c5d4b1180db6e00e687c5bb022112c790506e632
ligrew <ligrew@gmail.com>
2012-12-18 09:28:54 -0800

    Projekt 3 Visual Studio

--- proj_3/VS/dwudziestobok.cpp
+++ proj_3/VS/dwudziestobok.cpp
@@ -89,6 +89,62 @@ void ChangeSize(int w, int h)
 	viewFrustum.SetPerspective(fov,(float)(w/h),1.0f,50.0f);	//fov, ratio, blisko, daleko
 }
 
+
+
+//----------------------------------------------------
+void TriangleFace(M3DVector3f a, M3DVector3f b, M3DVector3f c) {
+   M3DVector3f normal, bMa, cMa;
+   m3dSubtractVectors3(bMa, b, a);
+   m3dSubtractVectors3(cMa, c, a);
+   m3dCrossProduct3(normal, bMa, cMa);
+   m3dNormalizeVector3(normal);
+   glVertexAttrib3fv(GLT_ATTRIBUTE_NORMAL, normal);
+   glVertex3fv(a);
+   glVertex3fv(b);
+   glVertex3fv(c);
+}
+
+//----------------------------------------------------
+void drawTriangles(int n_faces, float *vertices, int *faces) {
+	for (int i = 0; i < n_faces; i++) {
+		glBegin(GL_TRIANGLES);
+		TriangleFace(vertices + 3 * faces[3 * i], vertices + 3 * faces[3 * i + 1], vertices + 3 * faces[3 * i + 2]);
+		glEnd();
+    }
+}
+ 
+//----------------------------------------------------
+void drawSmoothTriangles(int n_faces, float *vertices, int *faces) {
+    M3DVector3f normal;
+    for (int i = 0; i < n_faces; i++) {
+		glBegin(GL_TRIANGLES);
+    	for(int j=0;j<3;++j) {
+			m3dCopyVector3(normal,vertices+3*faces[i*3+j]);
+			m3dNormalizeVector3(normal);
+    		glVertexAttrib3fv(GLT_ATTRIBUTE_NORMAL, normal);
+    		glVertex3fv(vertices+3*faces[i*3+j]);
+		}
+    glEnd();
+    }
+}
+
+//----------------------------------------------------
+void drawGrid() {
+	glBegin(GL_LINES);
+	for(float i=-10.0; i<=10.0; i++)
+	{
+		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 1.0f, 1.0f, 1.0f);
+		glVertex3f(-10.0f, i, 0.0f);
+		glVertex3f( 10.0f, i, 0.0f);
+		
+		glVertex3f(i, -10.0f, 0.0f);
+		glVertex3f(i,  10.0f, 0.0f);
+	}
+    glEnd();
+}
+
+
+
 ///////////////////////////////////////////////////////////////////////////////
 // This function does any needed initialization on the rendering context.
 // This is the first opportunity to do any OpenGL related tasks.
@@ -98,100 +154,24 @@ void SetupRC()
     glClearColor(0.097f, 0.184f, 0.199f, 1.0f);// szaro/niebieskie tło assassin
 	
     glEnable(GL_DEPTH_TEST);
-	
-    shader = gltLoadShaderPairWithAttributes("shader.vp", "shader.fp", 2, GLT_ATTRIBUTE_VERTEX, "vVertex", GLT_ATTRIBUTE_NORMAL, "vNormal");
+
+	shader = gltLoadShaderPairWithAttributes("shader.vp", "shader.fp", 2, GLT_ATTRIBUTE_VERTEX, "vVertex", GLT_ATTRIBUTE_NORMAL, "vNormal");
     fprintf(stdout, "GLT_ATTRIBUTE_VERTEX : %d\nGLT_ATTRIBUTE_COLOR : %d \n", GLT_ATTRIBUTE_VERTEX, GLT_ATTRIBUTE_COLOR);
-
-    MVMatrixLocation = glGetUniformLocation(shader,"MVMatrix");
-    MVPMatrixLocation = glGetUniformLocation(shader,"MVPMatrix");
-    normalMatrixLocation = glGetUniformLocation(shader,"normalMatrix");
-    ambientLightLocation = glGetUniformLocation(shader,"ambientLight");
-    shaderPositionLocation = glGetUniformLocation(shader, "light1.position");
-    shaderColorLocation = glGetUniformLocation(shader, "light1.color");
-    shaderAngleLocation = glGetUniformLocation(shader, "light1.angle");
-    shaderAttenuation0Location = glGetUniformLocation(shader, "light1.attenuation0");
-    shaderAttenuation1Location = glGetUniformLocation(shader, "light1.attenuation1");
-    shaderAttenuation2Location = glGetUniformLocation(shader, "light1.attenuation2");
-    shaderAmbientColorLocation = glGetUniformLocation(shader, "material.ambientColor");
-    shaderDiffuseColorLocation = glGetUniformLocation(shader, "material.diffuseColor");
-    shaderSpecularColorLocation = glGetUniformLocation(shader, "material.specularColor");
-    shaderSpecularExponentLocation = glGetUniformLocation(shader, "material.specularExponent");
-	if(MVPMatrixLocation==-1)
-		fprintf(stderr,"Nie mozna znalezc macierzy MVPMatrix\n");
+	
+	MVPMatrixLocation = glGetUniformLocation(shader,"MVPMatrix");
+	normalMatrixLocation = glGetUniformLocation(shader,"normalMatrix");
+	MVMatrixLocation = glGetUniformLocation(shader,"MVMatrix");
+	shaderPositionLocation = glGetUniformLocation(shader, "light1.position");
+	shaderColorLocation = glGetUniformLocation(shader, "light1.color");
+	shaderAngleLocation = glGetUniformLocation(shader, "light1.angle");
+	shaderAttenuation0Location = glGetUniformLocation(shader, "light1.attenuation0");
+	shaderAttenuation1Location = glGetUniformLocation(shader, "light1.attenuation1");
+	shaderAttenuation2Location = glGetUniformLocation(shader, "light1.attenuation2");
+	shaderAmbientColorLocation = glGetUniformLocation(shader, "material.ambientColor");
+	shaderDiffuseColorLocation = glGetUniformLocation(shader, "material.diffuseColor");
+	shaderSpecularColorLocation = glGetUniformLocation(shader, "material.specularColor");
+	shaderSpecularExponentLocation = glGetUniformLocation(shader, "material.specularExponent");
 }
-
-
-
-void rysujOstroslup()
-{
-	glBegin(GL_TRIANGLES);
-		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.5, 0.0, 0.0);//przod
-		
-		glVertex3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, 1.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, 2.0f);
-		
-		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.0, 0.5, 0.0);//tyl
-		
-		glVertex3f(-1.0f, -1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, 2.0f);
-		
-		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.0, 0.0, 0.5);//prawa
-		
-		glVertex3f(1.0f, -1.0f, 0.0f);
-		glVertex3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, 2.0f);
-		
-		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.0, 0.5, 0.5);//lewa
-		
-		glVertex3f(-1.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, -1.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, 2.0f);
-		
-		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.5, 0.5, 0.0);//dno prawe
-		
-		glVertex3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, 0.0f);
-		glVertex3f(-1.0f, 1.0f, 0.0f);
-		
-		glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.5, 0.5, 5.0);//dno lewe
-		
-		glVertex3f(-1.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, -1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, 0.0f);
-	glEnd();
-}
-
-void rysujSiatke()
-{
-    glBegin(GL_LINES);
-
-    glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.0f, 0.0f, 0.0f);
-
-    for(int i=-10;i<11;i++)
-        for(int j=-10; j<11;j++)
-		{
-            glVertex3f(i, j, 0.0f);
-            glVertex3f(-i, j, 0.0f);
-            glVertex3f(i, -j, 0.0f);
-            glVertex3f(i, j, 0.0f);
-        }
-		
-    glVertexAttrib3f(GLT_ATTRIBUTE_COLOR, 0.7f, 0.7f, 0.7f);
-
-    for(float i=-10;i<10.25;i+=0.25)
-        for(float j=-10; j<10.25;j+=0.25)
-			if (fmod(i,1) != 0 && fmod(j,1) != 0)
-			{
-				glVertex3f(i, j, 0.0f);
-				glVertex3f(-i, j, 0.0f);
-				glVertex3f(i, -j, 0.0f);
-				glVertex3f(i, j, 0.0f);
-			}
-    glEnd();
-}
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // Called to draw scene
@@ -207,7 +187,6 @@ void RenderScene(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     //glFrontFace(GL_CW);
     glUseProgram(shader);
-
 	
 	float angle=timer.GetElapsedSeconds()*M_PI/5;
 
@@ -220,11 +199,8 @@ void RenderScene(void) {
 
 	LookAt(cameraFrame,eye,at,up);
 	
-
 	geometryPipeline.SetMatrixStacks(modelView,projection);
 
-
-	
 	//swiatlo
 	M3DVector3f ambientLight = {1.0f, 1.0f, 1.0f};
 	M3DVector3f position = {2.0f, 2.0f, 5.0f};
@@ -238,8 +214,43 @@ void RenderScene(void) {
 	M3DVector3f specularColor = {1.0f, 1.0f, 1.0f};
 	float specularExponent = 9;
 	
+	float ico_vertices[3 * 12] = {
+		 0., 0., -0.9510565162951536,
+		 0., 0., 0.9510565162951536,
+		-0.85065080835204, 0., -0.42532540417601994,
+		 0.85065080835204, 0., 0.42532540417601994,
+		 0.6881909602355868, -0.5, -0.42532540417601994,
+		 0.6881909602355868, 0.5, -0.42532540417601994,
+		-0.6881909602355868, -0.5, 0.42532540417601994,
+		-0.6881909602355868, 0.5, 0.42532540417601994,
+		-0.2628655560595668, -0.8090169943749475, -0.42532540417601994,
+		-0.2628655560595668, 0.8090169943749475, -0.42532540417601994,
+		 0.2628655560595668, -0.8090169943749475, 0.42532540417601994,
+		 0.2628655560595668, 0.8090169943749475, 0.42532540417601994};
+
+	int ico_faces[3*20] = {
+		1 ,			 11 ,			 7 ,
+		1 ,			 7 ,			 6 ,
+		1 ,			 6 ,			 10 ,
+		1 ,			 10 ,			 3 ,
+		1 ,			 3 ,			 11 ,
+		4 ,			 8 ,			 0 ,
+		5 ,			 4 ,			 0 ,
+		9 ,			 5 ,			 0 ,
+		2 ,			 9 ,			 0 ,
+		8 ,			 2 ,			 0 ,
+		11 ,		 9 ,			 7 ,
+		7 ,			 2 ,			 6 ,
+		6 ,			 8 ,			 10 ,
+		10 ,		 4 ,			 3 ,
+		3 ,			 5 ,			 11 ,
+		4 ,			 10 ,			 8 ,
+		5 ,			 3 ,			 4 ,
+		9 ,			 11 ,			 5 ,
+		2 ,			 7 ,			 9 ,
+		8 ,			 6 ,			 2 };
+
 	projection.LoadMatrix(viewFrustum.GetProjectionMatrix());
-	modelView.PushMatrix();
 
 	cameraFrame.GetCameraMatrix(mCamera);
 	modelView.LoadMatrix(mCamera);
@@ -248,10 +259,9 @@ void RenderScene(void) {
 	glUniformMatrix4fv(MVMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewProjectionMatrix());
 	glUniformMatrix4fv(MVPMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewMatrix());
 	glUniformMatrix3fv(normalMatrixLocation,1,GL_FALSE,geometryPipeline.GetNormalMatrix());
-	glUniform3fv(ambientLightLocation, 1, ambientLight);
 	glUniform3fv(shaderPositionLocation, 1, position);
 	glUniform3fv(shaderColorLocation, 1, color);
-	glUniform1f(shaderAngleLocation, l_angle);
+	glUniform1f(shaderAngleLocation, angle);
 	glUniform1f(shaderAttenuation0Location, attenuation0);
 	glUniform1f(shaderAttenuation1Location, attenuation1);
 	glUniform1f(shaderAttenuation2Location, attenuation2);
@@ -259,16 +269,18 @@ void RenderScene(void) {
 	glUniform3fv(shaderDiffuseColorLocation, 1, diffuseColor);
 	glUniform3fv(shaderSpecularColorLocation, 1, specularColor);
 	glUniform1f(shaderSpecularExponentLocation, specularExponent);
-
-	rysujSiatke();
-	
+    glPolygonOffset(1.0f, 1.0f);
+	drawGrid(); 
+	glEnable(GL_POLYGON_OFFSET_FILL);
+	drawGrid(); 
+	glDisable(GL_POLYGON_OFFSET_FILL);
+	modelView.Translate(-2.0f,-2.0f,2.0f);
 	glUniformMatrix4fv(MVMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewProjectionMatrix());
 	glUniformMatrix4fv(MVPMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewMatrix());
 	glUniformMatrix3fv(normalMatrixLocation,1,GL_FALSE,geometryPipeline.GetNormalMatrix());
-	glUniform3fv(ambientLightLocation, 1, ambientLight);
 	glUniform3fv(shaderPositionLocation, 1, position);
 	glUniform3fv(shaderColorLocation, 1, color);
-	glUniform1f(shaderAngleLocation, l_angle);
+	glUniform1f(shaderAngleLocation, angle);
 	glUniform1f(shaderAttenuation0Location, attenuation0);
 	glUniform1f(shaderAttenuation1Location, attenuation1);
 	glUniform1f(shaderAttenuation2Location, attenuation2);
@@ -276,48 +288,16 @@ void RenderScene(void) {
 	glUniform3fv(shaderDiffuseColorLocation, 1, diffuseColor);
 	glUniform3fv(shaderSpecularColorLocation, 1, specularColor);
 	glUniform1f(shaderSpecularExponentLocation, specularExponent);
-
-	rysujOstroslup();
-
-	for (int i=-9; i<10; i+=3)
-		for (int j=-9; j< 10; j+=3)
-			if ((i < -2 || i > 2) && (j < -2 || j > 2))
-			{
-				modelView.PopMatrix();
-				modelView.PushMatrix();
-
-				modelView.Translate(i,j,0.0f);
-
-				glUniformMatrix4fv(MVMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewProjectionMatrix());
-				glUniformMatrix4fv(MVPMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewMatrix());
-				glUniformMatrix3fv(normalMatrixLocation,1,GL_FALSE,geometryPipeline.GetNormalMatrix());
-				glUniform3fv(ambientLightLocation, 1, ambientLight);
-				glUniform3fv(shaderPositionLocation, 1, position);
-				glUniform3fv(shaderColorLocation, 1, color);
-				glUniform1f(shaderAngleLocation, l_angle);
-				glUniform1f(shaderAttenuation0Location, attenuation0);
-				glUniform1f(shaderAttenuation1Location, attenuation1);
-				glUniform1f(shaderAttenuation2Location, attenuation2);
-				glUniform3fv(shaderAmbientColorLocation, 1, ambientColor);
-				glUniform3fv(shaderDiffuseColorLocation, 1, diffuseColor);
-				glUniform3fv(shaderSpecularColorLocation, 1, specularColor);
-				glUniform1f(shaderSpecularExponentLocation, specularExponent);
-
-				rysujOstroslup();
-			}
-
+	drawTriangles(20, ico_vertices, ico_faces);
 	modelView.PopMatrix();
 	modelView.PushMatrix();
-	modelView.Translate(1,0.0,2.0);
-	modelView.Rotate(180.0,180.0,0,1);
-
+	modelView.Translate(2.0,2.0,2.0);
 	glUniformMatrix4fv(MVMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewProjectionMatrix());
 	glUniformMatrix4fv(MVPMatrixLocation,1,GL_FALSE,geometryPipeline.GetModelViewMatrix());
 	glUniformMatrix3fv(normalMatrixLocation,1,GL_FALSE,geometryPipeline.GetNormalMatrix());
-	glUniform3fv(ambientLightLocation, 1, ambientLight);
 	glUniform3fv(shaderPositionLocation, 1, position);
 	glUniform3fv(shaderColorLocation, 1, color);
-	glUniform1f(shaderAngleLocation, l_angle);
+	glUniform1f(shaderAngleLocation, angle);
 	glUniform1f(shaderAttenuation0Location, attenuation0);
 	glUniform1f(shaderAttenuation1Location, attenuation1);
 	glUniform1f(shaderAttenuation2Location, attenuation2);
@@ -325,15 +305,9 @@ void RenderScene(void) {
 	glUniform3fv(shaderDiffuseColorLocation, 1, diffuseColor);
 	glUniform3fv(shaderSpecularColorLocation, 1, specularColor);
 	glUniform1f(shaderSpecularExponentLocation, specularExponent);
-
-	
-	rysujOstroslup();
-	modelView.PopMatrix();
+	drawSmoothTriangles(20, ico_vertices, ico_faces);
 	modelView.PopMatrix();
 
-
-    // Perform the buffer swap to display back buffer
-	
     glutSwapBuffers();
 	glutPostRedisplay();
 	
